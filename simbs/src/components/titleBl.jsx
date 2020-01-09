8802a30e21d902902740a4fc4c53a9a34c6782ea
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-09 13:56:29 +0500

    delete

--- simbs/src/components/titleBl.jsx
+++
@@ -1,7 +0,0 @@
-import React  from 'react';
-
-export default function TitleBlock(){
-    const words = ['commerce', 'offers', 'products'];
-    const items = words.map((titl)=> <p>{titl}</p>);
-    return(<div>{items}</div>, document.getElementById("blockTitle"))
-}

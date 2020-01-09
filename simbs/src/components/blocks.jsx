623605b9fc3fc0b69004196749c08c309978b832
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-09 14:32:17 +0500

    Update blocks.jsx

--- simbs/src/components/blocks.jsx
+++ simbs/src/components/blocks.jsx
@@ -6,29 +6,25 @@ import tayga from '../img/tayga.jpg';
 
 
 export default class Blocks extends Component {
-//     constructor(props){
-//         super(props)
-//         initialState = {
-//         currentId:0,
-//         blockS:[
-//             {
-//                 title:'commerce',
-//                 text:'ldiuf hsdk fhosdf jsdof pjoify'
-//             },
-//             {
-//                 title:'offers',
-//                 text:'oqwer tyui oasd fghjk'
-//             },
-//             {
-//                 title:'products',
-//                 text:'nbvcxz asd fghjk lpoi uytrew'
-//             }
-//         ]
-//     }
-//     updateValues();{
-//         this.initialState({title: 'comm'})
-//     }
-// }
+    constructor(props){
+        super(props)
+        this.initialState = {
+
+            title:['Altay',
+                    'Ural',
+                    'Tayga'],
+            text:[
+                'ldiuf hsdk fhosdf jsdof pjoify',
+                'oqwer tyui oasd fghjk',
+                'nbvcxz asd fghjk lpoi uytrew'
+            ],
+            img:[
+                altay,
+                ural,
+                tayga
+            ]
+        }
+    }
 
 
     render(){
@@ -36,17 +32,15 @@ export default class Blocks extends Component {
             <div  className="container container__first">
                 <div className="title__section">Offers</div>
                 <div className="blockLine col">
-                <div class="row align-items-center">
-                    <div class="col">
-                        <Block  />
+                    <div class="row align-items-center">
+                        {
+                            this.initialState.text.map((txt, id) =>
+                            <div class="col">
+                                <Block foto={this.initialState.img[id]} title={this.initialState.title[id]} text={this.initialState.text[id]}/>
+                            </div>
+                            )
+                        }
                     </div>
-                    <div class="col">
-                        <Block />
-                    </div>
-                    <div class="col">
-                        <Block />
-                    </div>
-                </div>
                 </div>
             </div>
         )

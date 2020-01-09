4319eb5ecf543bba26cce451a85b98cafc066a65
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-09 13:43:34 +0500

    reorganization -> func

--- simbs/src/components/formBlock.jsx
+++ simbs/src/components/formBlock.jsx
@@ -1,18 +1,17 @@
 import React, {Component} from 'react'
 // import { valueToNode } from '@babel/types'
 
-export default class FormBlock extends Component {
-    render(){
-        return(
-            <div id= "form_content" className="form_content">
-                <form action="input">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Text</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-            </form>
+export default function FormBlock(props) {
+    return(
+        <div id= "form_content" className="form_content">
+            <form action="input">
+            <div class="form-group">
+                <label for="exampleInputEmail1"></label>
+                <input type="email" class="form-control" id={props.id} aria-describedby="emailHelp" placeholder={props.placeholder}></input>
+                <small  class="form-text text-muted">{props.text}</small>
             </div>
-        );
-    }
+        </form>
+        </div>
+    );
 }
+
